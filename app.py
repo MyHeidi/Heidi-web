763724c3c34fcd28b6d5ffcffe04f538d77036c3
Marc Gähwiler <marc@marcg.ch>
2016-05-14 18:51:47 +0100

    ...

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ def home():
     return "done"
 
 
-@app.route('/update', methods=['GET', 'POST'])
-def update():
+@app.route('/update_location', methods=['GET', 'POST'])
+def updateLocation():
     if request.method == 'POST':
         lng = request.form.get('lng')
         lat = request.form.get('lat')
@@ -25,33 +25,50 @@ def update():
     print("Data: lng: {}, lat: {}".format(lng, lat))
     action = "notification"
     message = "You arrived at London Gatwick Airport"
-    questions = [{
-        "question": "You arrived at London Gatwick Airport. Do you need some assistance?",
-        "answers": [{
-            "answer": "hotel_route",  # Hotel
-            "action": "route",  # question, maps, uber, social, null
-            "location": {
-                "lat": -0.07034167,
-                "lng": 51.510425,
-            }
-        }, {
-            "answer": "phone_charges",  # Phone
-            "action": "question",  # null
-            "question": {
-                "question": "Roaming charges (CHF):\nTo CH: 2.- per min\nLocal: 1.20 per min\nIncomming: 1.- per min\nSMS: 0.45\nData: 2.- per MB\nActivate Go Europe?",
-                "answers": [{
-                    "answer": "yes",
-                    "action": "request",
-                    "url": "http://dev.heidi.wx.rs/action/phone_charges/go_europe",
-                }, {
-                    "answer": "no",
-                    "action": None,
-                }],
-            }
-        }, {
-            "answer": "country_info",  # Phone
-            "action": "url",  # null
-            "url": "https://en.wikipedia.org/wiki/Gatwick_Airport",
-        }],
-    }]
-    return jsonify(action=action, message=message, questions=questions)
+    return jsonify(action=action, message=message)
+
+
+@app.route('/get_question')
+def get_question():
+    if request.method == 'POST':
+        lng = request.form.get('lng', 0)
+        lat = request.form.get('lat', 0)
+        prev_answers = request.form.get('prev_answers')
+    else:
+        lng = 0
+        lat = 0
+        prev_answers = []
+    action = "question"
+    id = "q_airport"
+    question = "You arrived at London Gatwick Airport. Do you need some assistance?",
+    answers = [{
+        "id": "a_airport_hotel_route",
+        "answer": "hotel_route",  # Hotel
+        "action": "route",
+        "location": {
+            "lat": -0.07034167,
+            "lng": 51.510425,
+        }
+    }, {
+        "id": "a_airport_phone_charges",
+        "answer": "phone_charges",  # Phone
+        "action": "question",  # null
+    }, {
+        "id": "a_airport_country_info",
+        "answer": "country_info",  # Phone
+        "action": "url",  # null
+        "url": "https://en.wikipedia.org/wiki/Gatwick_Airport",
+    }],
+    """"questions": [{
+            "question": "Roaming charges (CHF):\nTo CH: 2.- per min\nLocal: 1.20 per min\nIncomming: 1.- per min\nSMS: 0.45\nData: 2.- per MB\nActivate Go Europe?",
+            "answers": [{
+                "answer": "yes",
+                "action": "request",
+                "url": "http://dev.heidi.wx.rs/action/phone_charges/go_europe",
+            }, {
+                "answer": "no",
+                "action": None,
+            }],
+        }],"""
+
+    return jsonify(action=action, id=id, question=question, answers=answers)
