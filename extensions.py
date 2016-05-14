dc7b71abe6f2225003e3a7b633c284b1c58b2135
Marc Gähwiler <marc@marcg.ch>
2016-05-14 17:46:46 +0100

    ...

--- extensions.py
+++ extensions.py
@@ -2,8 +2,8 @@
 """Extensions module. Each extension is initialized in the app factory located in app.py."""
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CsrfProtect
+# from flask_wtf.csrf import CsrfProtect
 
-csrf_protect = CsrfProtect()
+# csrf_protect = CsrfProtect()
 db = SQLAlchemy()
 debug_toolbar = DebugToolbarExtension()
